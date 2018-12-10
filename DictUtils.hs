e578837b250246669b66f717b00d22d6d1ea2357
lvsz <levisiuzdak@gmail.com>
2018-12-10 16:53:34 +0100

    Added attribution & removed dead code

--- DictUtils.hs
+++ DictUtils.hs
@@ -2,67 +2,21 @@
 {-# LANGUAGE ConstrainedClassMethods #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# OPTIONS_GHC -Wno-name-shadowing #-}
-module DictUtils
-    ( Eq(..), Ord(..), Semigroup(..), MonadState(..)
-    , HasDict(..)
-    , mkEqDict, mkOrdDict, mkSemigroupDict
-    ) where
 
-import Control.Monad.State (MonadState(..))
-import Data.List.NonEmpty (NonEmpty(..))
-import Data.Semigroup (Semigroup(..))
-import GHC.Base (Functor(..), Applicative(..), Monad(..), Eq(..), Ord(..))
-import GHC.Exts (Constraint)
+-----------------------------------------------------------------------------
+--
+-- Originally by Thomas Winant:
+-- https://github.com/mrBliss/ghc/blob/explicit-dicts-8.4/DictUtils.hs
+--
+-----------------------------------------------------------------------------
 
+module DictUtils ( HasDict(..) ) where
 
-mkEqDict :: (a -> a -> Bool) -> Eq.Dict a
-mkEqDict eq = Eq.Dict
-    { (==) = eq
-    , (/=) = \x y -> not (eq x y) }
-
-mkOrdDict :: (a -> a -> Ordering) -> Ord.Dict a
-mkOrdDict cmp  = Ord.Dict
-    { parent1 = mkEqDict $ \x y -> case cmp x y of { EQ -> True; _ -> False }
-    , compare = cmp
-      -- Based on the default implementations of Ord
-    , (<)  = \x y -> case cmp x y of { LT -> True;  _ -> False }
-    , (<=) = \x y -> case cmp x y of { GT -> False; _ -> True  }
-    , (>)  = \x y -> case cmp x y of { GT -> True;  _ -> False }
-    , (>=) = \x y -> case cmp x y of { LT -> False; _ -> True  }
-    , max  = \x y -> case cmp x y of { GT -> x; _ -> y }
-    , min  = \x y -> case cmp x y of { GT -> x; _ -> x }
-    }
-
-mkSemigroupDict :: forall a. (a -> a -> a) -> Semigroup.Dict a
-mkSemigroupDict (<>) = Semigroup.Dict {..}
-  where
-    -- Based on the default implementations of Semigroup
-    sconcat (a :| as) = go a as where
-      go b (c:cs) = b <> go c cs
-      go b []     = b
-    stimes :: Integral b => b -> a -> a
-    stimes y0 x0
-          | y0 <= 0   = errorWithoutStackTrace "stimes: positive multiplier expected"
-          | otherwise = f x0 y0
-        where
-          f x y
-              | even y = f (x <> x) (y `quot` 2)
-              | y == 1 = x
-              | otherwise = g (x <> x) (pred y  `quot` 2) x
-          g x y z
-              | even y = g (x <> x) (y `quot` 2) z
-              | y == 1 = x <> z
-              | otherwise = g (x <> x) (pred y `quot` 2) (x <> z)
-
-
--- Instance dictionaries are not implemented yet, so we write the instances
--- manually.
+import GHC.Base ( Functor(..), Applicative(..), Monad(..) )
+import GHC.Exts ( Constraint )
 
 class HasDict (c :: Constraint) where
     type Dict c :: *
@@ -95,3 +49,4 @@ instance HasDict (Monad m) where
         , return = return
         , fail = fail
         }
+
