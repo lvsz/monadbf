2066ce8a0d79ae9a7e5a219df726a9fd80f439aa
lvsz <levisiuzdak@gmail.com>
2018-12-27 23:44:34 +0100

    Added better option handling

--- main.hs
+++ main.hs
@@ -1,23 +1,85 @@
 import qualified Data.ByteString.Lazy as BS
-import Control.Monad      ( forM_ )
-import System.Environment ( getArgs )
-import System.IO          ( readFile )
+import Data.List             ( intersperse )
+import Control.Monad         ( forM_, join )
+import System.Console.GetOpt
+import System.Environment    ( getArgs )
+import System.Exit           ( ExitCode(..), exitWith )
+import System.IO             ( hPutStr, readFile, stderr )
 
 import BFPure
 import BFState
 import BFReaderT
 import BFDict
 
+data Mode = State
+          | Reader
+          | Dict
+          | Pure
+          deriving (Enum, Bounded, Show, Read)
+
+modes :: [Mode]
+modes = enumFromTo minBound maxBound
+
+run :: Mode -> String -> IO ()
+run mode = case mode of
+    State  -> runBFState
+    Reader -> runBFReaderT
+    Dict   -> runBFDict
+    Pure   -> \prog -> do
+        input <- BS.unpack <$> BS.getContents
+        BS.putStr $ runBFPure input prog
+
+data Options = Options { optHelp       :: Bool
+                       , optListModes  :: Bool
+                       , optMode       :: Mode
+                       , optTapeLength :: Int
+                       }
+
+defaultMode = Dict
+
+defaultTapeLength = 30000
+
+defaultOptions :: Options
+defaultOptions = Options { optHelp       = False
+                         , optListModes  = False
+                         , optMode       = defaultMode
+                         , optTapeLength = defaultTapeLength
+                         }
+
+options :: [OptDescr (Options -> Options)]
+options =
+    [ Option ['h'] ["help"]
+          (NoArg (\opts -> opts { optHelp = True }))
+          "Print this help message"
+    , Option [] ["list-modes"]
+          (NoArg (\opts -> opts { optListModes = True }))
+          "List available Brainfuck modes."
+    , Option ['m'] ["mode"]
+          (ReqArg (\x opts -> opts { optMode = read x }) "<MODE>")
+          ("Mode to use, default is " ++ show defaultMode)
+    , Option ['n'] ["tape-length"]
+          (ReqArg (\x opts -> opts { optTapeLength = read x }) "<NUM>")
+          ("Number of bytes in a tape, default is " ++ show defaultTapeLength)
+    ]
+  where
+    showModes = "<" ++ (join . intersperse "|" $ map show modes) ++ ">"
+
+parseOptions :: [String] -> IO (Options, [String])
+parseOptions argv = case getOpt RequireOrder options argv of
+    (opts, files, []) | optHelp opts'      -> putHelp
+                      | optListModes opts' -> listModes
+                      | otherwise          -> return (opts', files)
+                      where opts' = foldl (flip ($)) defaultOptions opts
+    (_, _, es) -> ioError $ userError $ concat es ++ usageInfo header options
+  where
+    header = "Usage: monadbf [OPTION...] [--] [files...]"
+    putHelp = hPutStr stderr (usageInfo header options) >> exit
+    listModes = putStr (unlines $ "Available modes:":map show modes) >> exit
+    exit = exitWith ExitSuccess
+
 main :: IO ()
 main = do
-    (mode:file:_) <- getArgs
-    prog <- readFile file
-    runBFState prog
-    case mode of
-        "State" -> runBFState prog
-        "ReaderT" -> runBFReaderT prog
-        "Dict" -> runBFDict prog
-        "Pure" -> do
-            input <- BS.unpack <$> BS.getContents
-            BS.putStr $ (runBFPure input prog)
+    (opts, args) <- getArgs >>= parseOptions
+    files <- mapM readFile args
+    forM_ files $ run (optMode opts)
 
