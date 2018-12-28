d6dfc1407920d6b2683f6a48aab7583c4b5a2938
lvsz <levisiuzdak@gmail.com>
2018-12-28 21:17:10 +0100

    Debugged & added simple lexing stage

--- MonadBF.hs
+++ MonadBF.hs
@@ -1,7 +1,6 @@
 module MonadBF ( MonadBF(..), parseBF ) where
 
 import Control.Monad.Loops ( whileM_ )
-import Data.Functor        ( ($>) )
 import Data.Word           ( Word8 )
 import Text.Parsec
 
@@ -16,30 +15,57 @@ class Monad m => MonadBF m where
     readByte :: m ()
     getByte  :: m Word8
 
-parseBFCommand :: MonadBF bf => Parsec String () (bf ())
-parseBFCommand =  char '+' `is` incByte
-              <|> char '-' `is` decByte
-              <|> char '<' `is` decPtr
-              <|> char '>' `is` incPtr
-              <|> char ','  $>  readByte
-              <|> char '.'  $>  showByte
-              <|> whileM_ ((/= 0) <$> getByte) <$> loop
-              <|> anyChar `parserBind` (unexpected . show)
-  where
-    c `is` f = f . fromIntegral . length <$> many1 c
-    loop = between (char '[') (char ']') $ foldl1 (>>) <$> cmds
-    cmds = let check [] = errorWithoutStackTrace "Error: non-terminating loop"
-               check xs = xs
-           in check <$> many parseBFCommand
+data BFCode = IncByte !Int
+            | DecByte !Int
+            | DecPtr  !Int
+            | IncPtr  !Int
+            | GetByte
+            | PutByte
+            | Loop    ![BFCode]
+            | Comment String
 
-isBFCommand :: Char -> Bool
-isBFCommand = flip elem "+-<>,.[]"
+instance Show BFCode where
+    show (IncByte n) = replicate n '+'
+    show (DecByte n) = replicate n '-'
+    show (DecPtr n) = replicate n '<'
+    show (IncPtr n) = replicate n '>'
+    show GetByte = ","
+    show PutByte = "."
+    show (Loop xs) = "[" ++ concatMap show xs ++ "]"
+    show (Comment s) = s
+
+isBFCommand :: BFCode -> Bool
+isBFCommand (Comment _) = False
+isBFCommand _           = True
+
+removeBFComments :: [BFCode] -> [BFCode]
+removeBFComments = filter isBFCommand
+
+bfCode :: Parsec String () BFCode
+bfCode =  IncByte . length <$> many1 (char '+')
+      <|> DecByte . length <$> many1 (char '-')
+      <|> DecPtr  . length <$> many1 (char '<')
+      <|> IncPtr  . length <$> many1 (char '>')
+      <|> GetByte <$  char ','
+      <|> PutByte <$  char '.'
+      <|> Loop    <$> between (char '[') (char ']') (many bfCode)
+      <|> Comment <$> many1 (noneOf "+-[]<>.,")
+
+evalBFCode :: MonadBF bf => BFCode -> bf ()
+evalBFCode cmd = case cmd of
+    IncByte n -> incByte (fromIntegral n)
+    DecByte n -> decByte (fromIntegral n)
+    DecPtr n  -> decPtr n
+    IncPtr n  -> incPtr n
+    GetByte   -> readByte
+    PutByte   -> showByte
+    Loop xs   -> let cmds = map evalBFCode $ removeBFComments xs
+                 in whileM_ ((/= 0) <$> getByte) (foldl1 (>>) cmds)
+    Comment _ -> return ()
 
 parseBF :: MonadBF bf => String -> bf ()
-parseBF s | null prog = return ()
-          | otherwise = either parseError id bf
-  where
-    prog = filter isBFCommand s
-    bf = parse (foldl1 (>>) <$> many parseBFCommand) "BF" prog
-    parseError err = errorWithoutStackTrace $ "Parsing error:\n" ++ show err
+parseBF s = case removeBFComments <$> parse (many bfCode) "BF" s of
+    Right [] -> return ()
+    Right bf -> foldl1 (>>) $ map evalBFCode bf
+    Left err -> errorWithoutStackTrace $ "Parsing error:\n" ++ show err
 
