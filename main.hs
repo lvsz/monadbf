bc2fdf560d1919ca6967aaa6d2b5f75b95ffd6fe
lvsz <levisiuzdak@gmail.com>
2022-04-01 04:12:36 +0200

    Improved readability

--- main.hs
+++ main.hs
@@ -50,35 +50,35 @@ defaultOptions = Options { optHelp       = False
 options :: [OptDescr (Options -> Options)]
 options =
     [ Option ['h'] ["help"]
-          (NoArg (\opts -> opts { optHelp = True }))
-          "Print this help message"
+        (NoArg (\opts -> opts { optHelp = True }))
+        "Print this help message"
     , Option [] ["list-modes"]
-          (NoArg (\opts -> opts { optListModes = True }))
-          "List available Brainfuck modes."
+        (NoArg (\opts -> opts { optListModes = True }))
+        "List available Brainfuck modes."
     , Option ['m'] ["mode"]
-          (ReqArg (\x opts -> opts { optMode = read x }) "<MODE>")
-          ("Mode to use, default is " ++ show defaultMode)
+        (ReqArg (\x opts -> opts { optMode = read x }) "<MODE>")
+        ("Mode to use, default is " ++ show defaultMode)
     , Option ['n'] ["tape-length"]
-          (ReqArg (\x opts -> opts { optTapeLength = read x }) "<NUM>")
-          ("Number of bytes in a tape, default is " ++ show defaultTapeLength)
+        (ReqArg (\x opts -> opts { optTapeLength = read x }) "<NUM>")
+        ("Number of bytes in a tape, default is " ++ show defaultTapeLength)
     ]
 
 parseOptions :: [String] -> IO (Options, [String])
 parseOptions argv = case getOpt RequireOrder options argv of
-    (opts, files, []) | optHelp opts'      -> putHelp
-                      | optListModes opts' -> putModes
-                      | otherwise          -> return (opts', files)
-                      where opts' = foldl (flip ($)) defaultOptions opts
-    (_, _, es) -> ioError $ userError $ concat es ++ usageInfo header options
+    (opts, files, []) -> let opts' = foldr ($) defaultOptions opts
+                         in if optHelp opts' then putHelp >> exit
+                            else if optListModes opts' then putModes >> exit
+                            else return (opts', files)
+    (_, _, es) -> ioError . userError $ concat es ++ usageInfo header options
   where
-    header = "Usage: monadbf [OPTION...] [--] [files...]"
-    putHelp = hPutStr stderr (usageInfo header options) >> exit
-    putModes = putStr (unlines $ "Available modes:":map show modes) >> exit
+    putHelp = hPutStr stderr (usageInfo header options)
+    putModes = putStr (unlines $ "Available modes:":map show modes)
     exit = exitWith ExitSuccess
+    header = "Usage: monadbf [OPTION...] [--] [files...]"
 
 main :: IO ()
 main = do
     (opts, args) <- getArgs >>= parseOptions
     files <- mapM readFile args
-    forM_ files $ run (optMode opts)
+    forM_ files (\file -> run (optMode opts) file)
 
